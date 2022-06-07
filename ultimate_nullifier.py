fba1867b24e41abc10d10fa092a1530b18b11802
Frostb1te <68353531+frostb1ten@users.noreply.github.com>
2022-06-07 09:33:28 -0500

    Create ultimate_nullifier.py

---
+++ ultimate_nullifier.py
@@ -0,0 +1,16 @@
+import time
+import os
+import sys
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+entity = sys.argv[1]
+for number in range(1,100):
+    time.sleep(0.1)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Starting Attack...")
+    print("--Hacking "+entity)
+    print("Penetrating into the system...")
+    print("Hacking "+entity+" "+str(number)+"%")
+os.system('cls' if os.name == 'nt' else 'clear')
+print(entity+" Hacked Successfully")
